e0c6ba2bd3b0d04120f6388d229a44fc76d51802
Brooklyn Zelenka <hello@brooklynzelenka.com>
2026-06-04 18:46:15 -0700

    Bup deps

--- flake.nix
+++ flake.nix
@@ -9,10 +9,10 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
     subduction.url = "github:inkandswitch/subduction";
     subduction.inputs.nixpkgs.follows = "nixpkgs";
